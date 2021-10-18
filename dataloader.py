16cf3c857f54aaef289305e66f17975ea42c0ffd
tsdlrh <2541002015@qq.com>
2021-10-18 10:01:40 +0800

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -4,11 +4,7 @@ import paddle
 import numpy as np
 import random
 import csv
-
 from paddle.io import Dataset, DataLoader, Sampler
-# from torchvision import transforms, utils
-# from torch.utils.data.sampler import Sampler
-
 from pycocotools.coco import COCO
 
 import skimage.io
@@ -24,10 +20,8 @@ class CocoDataset(Dataset):
 
     def __init__(self, root_dir, set_name='train2017', transform=None):
         """
-        Args:
-            root_dir (string): COCO directory.
-            transform (callable, optional): Optional transform to be applied
-                on a sample.
+        inputs: root_dir,set_name,transform
+        return 
         """
         self.root_dir = root_dir
         self.set_name = set_name
@@ -39,7 +33,7 @@ class CocoDataset(Dataset):
         self.load_classes()
 
     def load_classes(self):
-        # load class names (name -> label)
+        #加载标签label
         categories = self.coco.loadCats(self.coco.getCatIds())
         categories.sort(key=lambda x: x['id'])
 
@@ -51,7 +45,7 @@ class CocoDataset(Dataset):
             self.coco_labels_inverse[c['id']] = len(self.classes)
             self.classes[c['name']] = len(self.classes)
 
-        # also load the reverse (label -> name)
+        
         self.labels = {}
         for key, value in self.classes.items():
             self.labels[value] = key
@@ -80,28 +74,24 @@ class CocoDataset(Dataset):
         return img.astype(np.float32) / 255.0
 
     def load_annotations(self, image_index):
-        # get ground truth annotations
+       
         annotations_ids = self.coco.getAnnIds(imgIds=self.image_ids[image_index], iscrowd=False)
         annotations = np.zeros((0, 5))
 
-        # some images appear to miss annotations (like image with id 257034)
         if len(annotations_ids) == 0:
             return annotations
 
-        # parse annotations
         coco_annotations = self.coco.loadAnns(annotations_ids)
         for idx, a in enumerate(coco_annotations):
 
-            # some annotations have basically no width / height, skip them
             if a['bbox'][2] < 1 or a['bbox'][3] < 1:
                 continue
-
             annotation = np.zeros((1, 5))
             annotation[0, :4] = a['bbox']
             annotation[0, 4] = self.coco_label_to_label(a['category_id'])
             annotations = np.append(annotations, annotation, axis=0)
 
-        # transform from [x, y, w, h] to [x1, y1, x2, y2]
+        # 将 [x, y, w, h] 转为 [x1, y1, x2, y2]
         annotations[:, 2] = annotations[:, 0] + annotations[:, 2]
         annotations[:, 3] = annotations[:, 1] + annotations[:, 3]
 
@@ -161,43 +151,30 @@ def collater(data):
 
 
 class Resizer(object):
-    """Convert ndarrays in sample to Tensors."""
+    """调整Size"""
 
     def __call__(self, sample, min_side=608, max_side=1024):
         image, annots = sample['img'], sample['annot']
-
         rows, cols, cns = image.shape
-
         smallest_side = min(rows, cols)
-
-        # rescale the image so the smallest side is min_side
         scale = min_side / smallest_side
-
-        # check if the largest side is now greater than max_side, which can happen
-        # when images have a large aspect ratio
         largest_side = max(rows, cols)
 
         if largest_side * scale > max_side:
             scale = max_side / largest_side
 
-        # resize the image with the computed scale
         image = skimage.transform.resize(image, (int(round(rows * scale)), int(round((cols * scale)))))
         rows, cols, cns = image.shape
-
         pad_w = 32 - rows % 32
         pad_h = 32 - cols % 32
-
         new_image = np.zeros((rows + pad_w, cols + pad_h, cns)).astype(np.float32)
         new_image[:rows, :cols, :] = image.astype(np.float32)
-
         annots[:, :4] *= scale
 
         return {'scale': scale}
 
 
 class Augmenter(object):
-    """Convert ndarrays in sample to Tensors."""
-
     def __call__(self, sample, flip_x=0.5):
         if np.random.rand() < flip_x:
             image, annots = sample['img'], sample['annot']
@@ -218,65 +195,3 @@ class Augmenter(object):
         return sample
 
 
-class Normalizer(object):
-
-    def __init__(self):
-        self.mean = np.array([[[0.485, 0.456, 0.406]]])
-        self.std = np.array([[[0.229, 0.224, 0.225]]])
-
-    def __call__(self, sample):
-        image, annots = sample['img'], sample['annot']
-
-        return {'img': ((image.astype(np.float32) - self.mean) / self.std), 'annot': annots}
-
-
-class UnNormalizer(object):
-    def __init__(self, mean=None, std=None):
-        if mean == None:
-            self.mean = [0.485, 0.456, 0.406]
-        else:
-            self.mean = mean
-        if std == None:
-            self.std = [0.229, 0.224, 0.225]
-        else:
-            self.std = std
-
-    def __call__(self, tensor):
-        """
-        Args:
-            tensor (Tensor): Tensor image of size (C, H, W) to be normalized.
-        Returns:
-            Tensor: Normalized image.
-        """
-        for t, m, s in zip(tensor, self.mean, self.std):
-            t.mul_(s).add_(m)
-        return tensor
-
-
-class AspectRatioBasedSampler(Sampler):
-
-    def __init__(self, data_source, batch_size, drop_last):
-        self.data_source = data_source
-        self.batch_size = batch_size
-        self.drop_last = drop_last
-        self.groups = self.group_images()
-
-    def __iter__(self):
-        random.shuffle(self.groups)
-        for group in self.groups:
-            yield group
-
-    def __len__(self):
-        if self.drop_last:
-            return len(self.data_source) // self.batch_size
-        else:
-            return (len(self.data_source) + self.batch_size - 1) // self.batch_size
-
-    def group_images(self):
-        # determine the order of the images
-        order = list(range(len(self.data_source)))
-        order.sort(key=lambda x: self.data_source.image_aspect_ratio(x))
-
-        # divide into groups, one group = one batch
-        return [[order[x % len(order)] for x in range(i, i + self.batch_size)] for i in
-                range(0, len(order), self.batch_size)]
